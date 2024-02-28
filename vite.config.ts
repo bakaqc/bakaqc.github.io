5ef29bdce0b061e90c1a7852c64b74866898bd4e
Đinh Quốc Chương <126387856+bakaqc@users.noreply.github.com>
2024-02-28 08:32:21 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/publish-react-with-github-page/'
+  base: '/portfolio/'
 })
