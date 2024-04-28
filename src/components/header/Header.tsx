d8cb5357b5c3f47f60c7391874311cfd99fec8e5
Quốc Chương <quocchuong2609@gmail.com>
2024-04-28 15:29:25 +0700

    fix active link in header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -10,25 +10,28 @@ const Header = () => {
     setActiveLink(target.getAttribute("href") ?? "");
   };
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            setActiveLink("#" + entry.target.id);
-          }
-        });
-      },
-      { threshold: 0.7 }
-    );
+useEffect(() => {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && entry.intersectionRatio >= 0.7) {
+          setActiveLink("#" + entry.target.id);
+        }
+      });
+    },
+    { threshold: 0.7 }
+  );
 
+  // Delay observing elements to allow browser to scroll to element specified in URL
+  setTimeout(() => {
     const sections = document.querySelectorAll("section");
     sections.forEach((section) => observer.observe(section));
 
     return () => {
       sections.forEach((section) => observer.unobserve(section));
     };
-  }, []);
+  }, 100); // Delay can be adjusted as needed
+}, []);
   return (
     <>
       <header className="header">
