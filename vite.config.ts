b4e2198f4ba0e3980a4aba558765a824ce3449e0
Quốc Chương <quocchuong2609@gmail.com>
2025-09-10 16:11:50 +0700

    update: format code for consistency in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: "/",
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler' // or "modern"
-      }
-    }
-  }
-})
+        api: "modern-compiler", // or "modern"
+      },
+    },
+  },
+});
